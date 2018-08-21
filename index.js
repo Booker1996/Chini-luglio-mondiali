f4bf8a41e101ba08efde1e78e42489d4800e9ca5
Ubuntu 18.04 <lorenzo.chini@studenti.unitn.it>
2018-08-21 11:13:39 +0200

    Refactoring, aggiunta della struttura per la gestione delle richieste, aggiunta di moduli npm

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express'),
+    bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+
+
+const mondiali = require('./mondiali')
+
+
+app.use('/mondiali', mondiali)
+
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
--- mondiali.js
+++ mondiali.js
@@ -1,3 +1,10 @@
+const express = require('express'),
+    bodyParser = require('body-parser');
+const mondiali = express.Router()
+
+var uuid = require('uuid-v4');
+
+
 var matches = [];
 matches[0]={
     opponent: 'Croazia',
@@ -29,6 +36,7 @@ teams[1] = {
     is_still_in: false,
     matches: [matches[1],matches[3]]
 };
+//console.log(teams[0]);
+//console.log(teams[1]);
 
-console.log(teams[0]);
-console.log(teams[1]);
+module.exports = mondiali
